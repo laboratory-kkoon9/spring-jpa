6fd89608234587139d9852c0e58659fb4ffbe3b8
kkoon9 <rndrnjs2003@gmail.com>
2022-11-26 00:50:35 +0900

    feat: 영속성 전이 추가
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/com/laboratorykkoon9/springjpa/model/entity/Order.java
+++ src/main/java/com/laboratorykkoon9/springjpa/model/entity/Order.java
@@ -19,7 +19,7 @@ public class Order extends BaseEntity {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
@@ -31,10 +31,10 @@ public class Order extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
